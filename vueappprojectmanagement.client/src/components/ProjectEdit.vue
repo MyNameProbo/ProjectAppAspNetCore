6275aa0e333d8de77987288bdd020498d4c5d81b
probo sasongko <probo.sasongko@iverson.co.id>
2025-02-22 11:55:43 +0700

    fixing 2

--- vueappprojectmanagement.client/src/components/ProjectEdit.vue
+++ vueappprojectmanagement.client/src/components/ProjectEdit.vue
@@ -2,31 +2,32 @@
   <div class="container">
     <div class="row">
       <div class="col">
-        <form method="post">
+        <form method="post" @submit.prevent="edit()">
           <div class="row">
             <div class="mb-3 col-md-6 col-lg-4">
               <label class="form-label" for="project_name">Name</label>
-              <input id="project_name" name="Name" class="form-control" :value="project.name" maxlength="300" />
+              <input id="project_name" name="Name" class="form-control" v-model="project.name" maxlength="300" />
             </div>
             <div class="mb-3 col-md-6 col-lg-4">
               <label class="form-label" for="project_cust">Customer</label>
-              <input id="project_cust" name="Customer" class="form-control" :value="project.customer" maxlength="50" />
+              <input id="project_cust" name="Customer" class="form-control" v-model="project.customer" maxlength="50" />
             </div>
             <div class="mb-3 col-md-6 col-lg-4">
               <label class="form-label" for="project_start">Start date</label>
-              <input id="project_start" name="StartDate" class="form-control" :value="project.startDate" type="datetime" />
+              <input id="project_start" name="StartDate" class="form-control" v-model="project.startDate" type="datetime" />
             </div>
             <div class="mb-3 col-md-6 col-lg-4">
               <label class="form-label" for="project_end">End date</label>
-              <input id="project_end" name="EndDate" class="form-control" :value="project.endDate" type="datetime" />
+              <input id="project_end" name="EndDate" class="form-control" v-model="project.endDate" type="datetime" />
             </div>
             <div class="mb-3 col-md-6 col-lg-4">
               <label class="form-label" for="project_pm">Project manager</label>
-              <input id="project_pm" name="Manager" class="form-control" :value="project.projectManager" maxlength="50" />
-            </div>            
+
+              <input id="project_pm" name="Manager" class="form-control" v-model="project.projectManager" maxlength="50" />
+            </div>
             <div class="col-12">
               <router-link class="btn btn-secondary" to="/project">Back</router-link>
-              <router-link class="btn btn-primary" :to="'/projectedit/${project.projectId}'">Edit</router-link>
+              <button class="btn btn-primary">Submit</button>
             </div>
           </div>
         </form>
@@ -51,6 +52,13 @@ export default {
     get() {
       return Service.get(this.$route.params.id).then(response => {
         this.project = response.data
+      })
+    },
+    edit() {
+      Service.edit(this.$route.params.id, this.project).then(() => {
+        this.$router.push('/project')
+      }).catch((e) => {
+        alert(e.response.data)
       })
     }
   }
